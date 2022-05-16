d1216219a0ca3e51eb54e8c404706af4fd43ea3f
is3388 <meiychin@yahoo.com>
2022-05-16 18:26:40 -0400

    Created routers for GET, POST, PUT/PATCH and DELETE.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const bootcamps = require('./routes/bootcamps')
 
 // since this config file is not .env on the root, needs to specify path
 dotenv.config({path: './config/config.env'})
 const app = express()
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
