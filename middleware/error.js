09ceb9bdbc08769ed1f26d8e72a81db08830d282
is3388 <meiychin@yahoo.com>
2022-05-18 10:27:12 -0400

    Added use cases for Mongoose handling duplicate field and validation.

--- middleware/error.js
+++ middleware/error.js
@@ -5,7 +5,8 @@ const ErrorResponse = require('../utils/errorResponse')
 const errorHandler = (err, req, res, next) =>
 {
     // log to console for developer
-    console.log(err.stack.red)
+    //console.log(err.stack.red)
+    console.log(err)
     let error = {...err}
     // Mongoose bad ObjectID
     if(err.name === 'CastError')
@@ -13,6 +14,20 @@ const errorHandler = (err, req, res, next) =>
         const message = `Resource not found with id of ${err.value}`
         error = new ErrorResponse(message, 404)
     }
+    // Mongoose duplicate key
+    if(err.code === 11000)
+    { 
+        const message = 'Duplicate field value'
+        error = new ErrorResponse(message, 400)
+    }
+
+    // Mongoose validation error instead of using Express Validator
+    if(err.name === 'ValidationError')
+    { 
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new ErrorResponse(message, 400)
+    }
+
     res.status(error.statusCode || 500).json({success: false, error: error.message || 'Server Error'})
 }
 
