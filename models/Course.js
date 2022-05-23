73c9ece830444703581b7e68ec5942c8fb15e1f0
is3388 <meiychin@yahoo.com>
2022-05-23 11:19:23 -0400

    Added static method and related middlewares to calculate average cost of a specific bootcamp and stored into database.

--- models/Course.js
+++ models/Course.js
@@ -39,4 +39,47 @@ const CourseSchema = new mongoose.Schema({
     
 })
 
+// create a static method to get avg of course tuition of a specific bootcamp, store into db
+CourseSchema.statics.getAverageCost = async function(bootcampId) 
+{
+    console.log('Calculating the average cost ...')
+    //obj is an aggregated object and steps are inside the [] pipeline and this refers to current model
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        }, // match whatever pass in bootcamp field and use group operator to arrange result by criteria which is to get the Id and the averageCost
+        {
+            $group: { _id: '$bootcamp',
+                  averageCost: { $avg: '$tuition' }
+                    }
+        }
+     ])
+    console.log(obj) // obj is an object in an array with id and averageCost
+    try
+    { // this.model(modelname) is to reference another model from this current model and update the avg cost for that bootcamp
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, 
+        {
+            averageCost: obj.length > 0 ? Math.ceil(obj[0].averageCost / 10) * 10 : 0
+        })
+    }
+    catch (err)
+    {
+        console.error(err)
+    }
+}
+
+// Call getAverageCost after saving a course
+CourseSchema.post('save', async function()
+{
+    await this.constructor.getAverageCost(this.bootcamp)
+    //const course = this // static method is run on model
+    //await course.getAverageCost(this.bootcamp)
+})
+
+// Call getAverageCost after removing a course to get the updated avg cost
+CourseSchema.post('remove', async function()
+{
+    await this.constructor.getAverageCost(this.bootcamp)
+}) 
+
 module.exports = mongoose.model('Course', CourseSchema)
