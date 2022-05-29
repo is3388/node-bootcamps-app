65b605a51fe0a435c84199b234918490c2cdbd23
is3388 <meiychin@yahoo.com>
2022-05-29 19:07:01 -0400

    Updated seeder to include Reviews data loaded up.

--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotenv.config({path: './config/config.env'})
 const Bootcamp = require('./models/Bootcamp')
 const Course = require('./models/Course')
 const User = require('./models/User')
+const Review = require('./models/Review')
 //connect to DB
 mongoose.connect(process.env.MONGODB_URL), {
     useUnifiedTopology: true,
@@ -20,6 +21,7 @@ mongoose.connect(process.env.MONGODB_URL), {
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/data/bootcamps.json`, 'utf-8'))
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/data/courses.json`, 'utf-8'))
 const users = JSON.parse(fs.readFileSync(`${__dirname}/data/users.json`, 'utf-8'))
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/data/reviews.json`, 'utf-8'))
 
 //import data into DB
 const importData = async() =>
@@ -29,6 +31,7 @@ const importData = async() =>
         await Bootcamp.create(bootcamps)
         await Course.create(courses)
         await User.create(users)
+        await Review.create(reviews)
         console.log('Data imported ...'.green.inverse)
         process.exit()
     }
@@ -45,6 +48,7 @@ const deletetData = async() =>
         await Bootcamp.deleteMany()
         await Course.deleteMany()
         await User.deleteMany()
+        await Review.deleteMany()
         console.log('Data destroyed ...'.red.inverse)
         process.exit()
     }
