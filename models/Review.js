33b378d76e360d6d81f59a36800f0cd79f4044b1
is3388 <meiychin@yahoo.com>
2022-05-29 20:44:55 -0400

    Added static method of Review model to generate average rating of a specific bootcamp before saving or removing a rating.

--- models/Review.js
+++ models/Review.js
@@ -37,4 +37,46 @@ const ReviewSchema = new mongoose.Schema({
 // prevent user from submitting one review per bootcamp
 ReviewSchema.index({bootcamp: 1, user: 1}, {unique: true})
 
+// create a static method to get avg of course tuition of a specific bootcamp, store into db
+ReviewSchema.statics.getAverageRating = async function(bootcampId) 
+{
+    //console.log('Calculating the average cost ...')
+    //obj is an aggregated object and steps are inside the [] pipeline and this refers to current model
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        }, // match whatever pass in bootcamp field and use group operator to arrange result by criteria which is to get the Id and the averageCost
+        {
+            $group: { _id: '$bootcamp',
+                  averageRating: { $avg: '$rating' }
+                    }
+        }
+     ])
+    //console.log(obj) // obj is an object in an array with id and averageCost
+    try
+    { // this.model(modelname) is to reference another model from this current model and update the avg cost for that bootcamp
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, 
+        {
+            averageRating: obj.length > 0 ? obj[0].averageRating : 0
+        })
+    }
+    catch (err)
+    {
+        console.error(err)
+    }
+}
+
+// Call getAverageRating after saving a rating
+ReviewSchema.post('save', async function()
+{
+    await this.constructor.getAverageRating(this.bootcamp)
+    })
+
+// Call getAverageRating after removing a rating to get the updated avg rating
+ReviewSchema.post('remove', async function()
+{
+    await this.constructor.getAverageRating(this.bootcamp)
+}) 
+
+
 module.exports = mongoose.model('Review', ReviewSchema)
